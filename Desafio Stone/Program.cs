1a919a8997ed8ff708a735153d5eecc6d72f26c7
Yan Alves Monteiro <Yan Alves Monteiro>
2016-11-01 01:34:21 -0200

    update no output

--- Desafio Stone/Program.cs
+++ Desafio Stone/Program.cs
@@ -12,23 +12,33 @@ namespace Desafio_Stone
     {
         static void Main(string[] args)
         {
+            WriteLine("Quantas preposições existem no texto?");
             int quantidadeDePreposicoesNoTexto = DescobrirQuantidadeDePreposicoesNoTexto(TEXTO_NO_IDIOMA_PREDATOR);
-            WriteLine($"Existem {quantidadeDePreposicoesNoTexto} preposições no texto");
+            WriteLine($"Resposta: Existem {quantidadeDePreposicoesNoTexto} preposições no texto\n");
 
             int quantidadeDeVerbosEmPrimeiraPessoa;
             int quantidadeDeVerbosNoTexto = DescobrirQuantidadeDeVerbosNoTexto(TEXTO_NO_IDIOMA_PREDATOR, out quantidadeDeVerbosEmPrimeiraPessoa);
-            WriteLine($"Há {quantidadeDeVerbosNoTexto} verbos no texto");
-            WriteLine($"Há {quantidadeDeVerbosEmPrimeiraPessoa} verbos em primeira pessoa no texto");
+            WriteLine("Quantos verbos existem no texto?");
+            WriteLine($"Resposta: Há {quantidadeDeVerbosNoTexto} verbos no texto\n");
+            WriteLine("E quantos verbos do texto estão em primeira pessoa?");
+            WriteLine($"Resposta: Há {quantidadeDeVerbosEmPrimeiraPessoa} verbos em primeira pessoa no texto\n");
 
-            WriteLine("Em seguida, a lista de vocabulário do texto:");
+            WriteLine("Como seria a lista de vocabulário para o texto?");
+            WriteLine("Resposta: \n");
             var vocabulario = CriarVocabularioPedrator(TEXTO_NO_IDIOMA_PREDATOR);
             foreach (var palavra in vocabulario)
                 Write($"{palavra} ");
+            WriteLine("");
+            WriteLine("");
 
             int quantidadeDeNumerosLindosDistintosNoTexto;
             int quantidadeDeNumerosBonitosDistintosNoTexto = DescobrirQuantidadeDeNumerosBonitosDistintosNoTexto(TEXTO_NO_IDIOMA_PREDATOR, out quantidadeDeNumerosLindosDistintosNoTexto);
-            WriteLine($"No texto, há {quantidadeDeNumerosBonitosDistintosNoTexto} números bonitos distintos.");
-            WriteLine($"No texto, há {quantidadeDeNumerosLindosDistintosNoTexto} números lindos distintos.");
+
+            WriteLine("E no texto, quantos números bonitos distintos existem?");
+            WriteLine($"Resposta: No texto, há {quantidadeDeNumerosBonitosDistintosNoTexto} números bonitos distintos.\n");
+
+            WriteLine("E no texto, quantos números lindos distintos existem?");
+            WriteLine($"Resposta: No texto, há {quantidadeDeNumerosLindosDistintosNoTexto} números lindos distintos.");
 
             ReadKey();
         }
